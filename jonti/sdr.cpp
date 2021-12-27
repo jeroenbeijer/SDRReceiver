4d8ad9597a89bc53cf3a4f1e9b0e988edf260647
jeroenbeijer <31091871+jeroenbeijer@users.noreply.github.com>
2021-12-27 11:26:51 +0100

    Update sdr.cpp

--- jonti/sdr.cpp
+++ jonti/sdr.cpp
@@ -21,6 +21,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 
 #include "sdr.h"
+#include <thread>
 #include <qstringlist.h>
 #include <qstring.h>
 #include <QtConcurrent/QtConcurrent>
